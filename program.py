9701ed0771eb4f09cf7283aa033bac59828dac7e
tilyes <tamas.ilyes23@gmail.com>
2021-04-08 21:07:48 +0200

    first prototype

--- program.py
+++ program.py
@@ -1,69 +1,34 @@
 #!/usr/bin/env python3
 import argparse
-import pickle
+import json
 
-from src import raster_preprocessor
-from src import sequence_separator
+from src import map_viewer
 
 parser = argparse.ArgumentParser(description='My program description')
 parser.add_argument('--input-path', type=str, help='Path of the local file containing the input data.')
-parser.add_argument('--window-size', type=int, default=50, help='Window size.')
-parser.add_argument('--nuts-level', type=int, default=0, help='Window size.')
+parser.add_argument('--load-type', type=str, help='Type of the source of the image data.')
+parser.add_argument('--rst-lst', type=str, help='List of rasters to create images from.')
 parser.add_argument('--output-path', type=str, help='Path of the local file where the output data should be written.')
 args = parser.parse_args()
 
 if __name__ == "__main__":
 
-    level = args.nuts_level
+    metadata = {}
+    output_list = []
 
-    rasters = {
-        'pop': {
-            'type': 'input',
-            'fn': args.input_path + '/pop_rst.tif'
-        },
-        'dem': {
-            'type': 'input',
-            'fn': args.input_path + '/dem_aggr_rst.tif'
-        },
-        'ww': {
-            'type': 'input',
-            'fn': args.input_path + '/ww_aggr_rst.tif'
-        },
-        'nuts': {
-            'type': 'output',
-            'fn': args.input_path + f'/nuts_rst{level}.tif'
-        },
-        'area': {
-            'type': 'aux',
-            'fn': args.input_path + f'/nuts_area_rst{level}.tif'
-        },
-        'icb': {
-            'type': 'aux',
-            'fn': args.input_path + '/icb_rst.tif'
-        }
-    }
-    window_size = args.window_size
-    padding_size = args.window_size
-
-    data_preprocessor = raster_preprocessor.DataPreprocessor(raster_config_dict = rasters, window_size = window_size)
-    rasters = data_preprocessor.create_preprocessed_data()
-
-    generator_sequences, output_value_dict = sequence_separator.create_separated_sequences(rasters, window_size, padding_size)
-
-    pickle.dump(
-        [rasters, generator_sequences, window_size, output_value_dict],
-        open(args.output_path + "/features.p", "wb")
-    )
-
-    metadata = {
-        'outputs' : [
-            {
+    if args.load_type == 'from_raster':
+        rst_list = args.rst_lst.split("/")
+        for raster_file in rst_list:
+            rst_fn = args.input_path + '/' + raster_file
+            pic_hash = map_viewer.get_base64_image(rst_fn, 1, False)
+            inner_dict = {
                 'type': 'web-app',
                 'storage': 'inline',
-                'source': '<img src="data:image/gif;base64,xxxxxxxxxxxxx..." />'
+                'source': f'<img src="data:image/png;base64,{pic_hash}" />'
             }
-        ]
-    }
+            output_list.append(inner_dict)
+
+    metadata['outputs'] = output_list
 
     with open('/mlpipeline-ui-metadata.json', 'w') as f:
         json.dump(metadata, f)
