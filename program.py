383a0750b0dd5a183b956a400422fbe2404ee7d3
tilyes <tamas.ilyes23@gmail.com>
2021-08-24 18:01:16 +0200

    added vrt viewr

--- program.py
+++ program.py
@@ -2,13 +2,13 @@
 import argparse
 import json
 
-from src import map_viewer
+from src import map_viewer, vrt_viewer
 
 parser = argparse.ArgumentParser(description='My program description')
 parser.add_argument('--input-path', type=str, help='Path of the local file containing the input data.')
 parser.add_argument('--load-type', type=str, help='Type of the source of the image data.')
 parser.add_argument('--rst-lst', type=str, help='List of rasters to create images from.')
-parser.add_argument('--output-path', type=str, help='Path of the local file where the output data should be written.')
+parser.add_argument('--resolution', type=int, help='Resolution of the output map.')
 args = parser.parse_args()
 
 if __name__ == "__main__":
@@ -21,6 +21,16 @@ if __name__ == "__main__":
         for raster_file in rst_list:
             rst_fn = args.input_path + raster_file
             pic_hash = map_viewer.get_base64_image(rst_fn, 1, False)
+            inner_dict = {
+                'type': 'web-app',
+                'storage': 'inline',
+                'source': f'<img src="data:image/png;base64,{pic_hash}" width="100%" />'
+            }
+            output_list.append(inner_dict)
+    elif args.load_type == 'from_vrt':
+        rst_list = args.rst_lst.split(";")
+        for output_type in rst_list:
+            pic_hash = vrt_viewer.get_base64_image(args.input_path, output_type, resolution = args.resolution)
             inner_dict = {
                 'type': 'web-app',
                 'storage': 'inline',
---
+++ src/vrt_viewer.py
@@ -0,0 +1,74 @@
+import base64
+import io
+import os
+from osgeo import gdal
+import rasterio
+from matplotlib import pyplot
+
+def get_base64_image_from_vrt(src_path, output_type, experiment_type = None, xy_resolution = 1000):
+
+    # src_path = '/home/jovyan/data-vol-1/SATELLITE/RESULTS/hu_raster'
+    # experiment_type = 'km area level 0 no border 50 continental'.replace(' ', '_')
+    # output_type = 'pred'
+
+    if experiment_type is not None:
+        full_path = '/'.join([src_path, experiment_type])
+        all_subdirs = [d for d in os.listdir(full_path) if os.path.isdir('/'.join([src_path, experiment_type, d]))]
+        latest_subdir = max(['/'.join([src_path, experiment_type, d]) for d in all_subdirs], key=os.path.getmtime)
+        full_path = latest_subdir
+    else:
+        full_path = src_path
+
+    file_list = []
+    for r, d, f in os.walk(full_path):
+        for file in f:
+            if file[-4:] == '.tif' and f'output_{output_type}' in file:
+                file_list.append(os.path.join(r, file))
+
+
+    if output_type in ['prob', 'detailed']: 
+        with rasterio.open(file_list[0].replace('prob','detailed')) as ds:
+            band_count = ds.count
+
+    vrt_fn = '/tmp/test.vrt'
+    if os.path.exists(vrt_fn):
+        os.remove(vrt_fn)
+
+    if output_type in ['prob', 'detailed']:
+        nodata = 1/band_count
+    else:
+        nodata = 0
+
+    vrt_options = gdal.BuildVRTOptions(
+        xRes = xy_resolution,
+        yRes = xy_resolution,
+        addAlpha=False,
+        srcNodata=nodata,
+        VRTNodata=nodata,
+        hideNodata = True
+    )
+    test_vrt = gdal.BuildVRT(
+        vrt_fn,
+        file_list,
+        options=vrt_options
+    )
+
+    test_vrt = None
+
+    band = 1
+
+    src = rasterio.open(vrt_fn)
+    pyplot.figure(figsize = (40,40))
+    data = src.read(band)
+    if output_type == 'pred':
+        pyplot.imshow(data, cmap='tab20')
+    else:
+        pyplot.imshow(data, cmap='pink')
+
+    pic_IObytes = io.BytesIO()
+    pyplot.savefig(pic_IObytes,  format='png')
+    pic_IObytes.seek(0)
+    pic_hash = base64.b64encode(pic_IObytes.read())
+    pic_hash = str(pic_hash)[2:-1]
+    pyplot.close()
+    return pic_hash
